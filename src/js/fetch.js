86397ebddf03d787d4f1a710a7fda9039a70d73f
TZaidel <8tzaidel@gmail.com>
2023-12-20 20:51:55 +0100

    edit branch

---
+++ src/js/fetch.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export async function fetchProductsAll(category) {
+    const response = await axios(`https://food-boutique.b.goit.study/api/products?keyword=Ac&category=${category}&page=1&limit=10`)
+    return response.data.results
+}
+
+export async function fetchProducts(select) {
+    const response = await axios(`https://food-boutique.b.goit.study/api/products/${select}`)
+    return response.data
+}
