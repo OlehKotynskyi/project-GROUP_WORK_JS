1eccd04dc7d22012fef09eabc4322cf99737fc2d
OlehKotynskyi <88795955+OlehKotynskyi@users.noreply.github.com>
2023-12-23 13:52:47 +0000

    Deploying to gh-pages from @ OlehKotynskyi/project-GROUP_WORK_JS@afaffe0adb8272c07e1850151a703445e88e35c6 🚀

--- assets/template-2a507105.js
+++ assets/template-2a507105.js
@@ -1,7 +1,7 @@
 (function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function c(n){return n.splice(2),n.map(({price:s,name:o,img:i,_id:e})=>`<li class="discount-list-item" id="${e}" >
             <div class="discount-card">
             <svg class="icon-styles discount-icon" width="14" height="14" > 
-      <use href="../img/sprite.svg#discount"> </use > 
+      <use href="../img/sprite.svg#icon-discount"> </use > 
       </svg > 
             <img class="discount-img" src="${i}"></div>
             <div class="discount-info">
@@ -24,4 +24,4 @@
             <span class="products-quantity"></span>
             <button class="more-btn">+</button>
         </li>`).join("")}export{c as a,a as c};
-//# sourceMappingURL=template-ae1beb7e.js.map
+//# sourceMappingURL=template-2a507105.js.map
