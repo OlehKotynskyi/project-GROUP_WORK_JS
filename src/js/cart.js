29da85a61979e8be538364b977f7c98da5ccfe14
culplate <culplate@gmail.com>
2023-12-25 05:51:06 +0100

    Cart: delete all + delete certain item

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,48 +1,76 @@
 import { createMarkupCart } from './template.js'
 const title = document.querySelector('.products-quantity-title')
 const totalPrice = document.querySelector('.products-total-price')
-const container = document.querySelector('.cart-container')
+const containerCart = document.querySelector('.cart-container')
+const cartWrapper = document.querySelector('.cart-desktop-wrapper')
+const emptyCart = document.querySelector('.cart-empty')
+const deleteAllBtn = document.querySelector('.del-all-btn')
 const KEY = 'products in cart'
 
-const products = JSON.parse(localStorage.getItem(KEY))
+renderCart();
 
-container.insertAdjacentHTML('beforeend', createMarkupCart(products))
-const productsQuantity = document.querySelectorAll('.products-quantity')
-products.forEach((el, index) => {
-    productsQuantity[index].textContent = el.quantity
-})
+// очистка всієї корзини (localStorage)
+deleteAllBtn.addEventListener('click', clearCart)
+function clearCart() {
+    localStorage.removeItem(KEY)
+    renderCart();
+}
 
-title.textContent = `Cart (${products.length})`
-const total = products.reduce((acc, {quantity, price}) => {
-    return acc += quantity * (price * 10)
-}, 0)
-totalPrice.textContent = `$${total / 10}`
+// якщо localStorage пустий, показувати розмітку пустої корзини
+// і приховувати розмітку товарів.
+// Викликається в cart.page.js
+export function renderCart() {
+    const products = JSON.parse(localStorage.getItem(KEY))
+    
+    if (products === null || products === undefined || products.length === 0) {
+        cartWrapper.style.display = 'none';
+        emptyCart.style.display = 'block';
+        containerCart.innerHTML = '';
+        title.textContent = 'Cart (0)'
+        totalPrice.textContent = '$0';
+    } else {
+        containerCart.innerHTML = '';
+        cartWrapper.style.display = 'flex';
+        emptyCart.style.display = 'none';
 
+        containerCart.insertAdjacentHTML('beforeend', createMarkupCart(products))
+        const productsQuantity = document.querySelectorAll('.products-quantity')
+    
+        products.forEach((el, index) => {
+            productsQuantity[index].textContent = el.quantity
+        })
+
+        title.textContent = `Cart (${products.length})`
+        const total = products.reduce((acc, {quantity, price}) => {
+            return acc += quantity * price
+        }, 0)
+        totalPrice.textContent = `$${total.toFixed(2)}`
+    }
+}
+
+// додавання, віднімання продуктів
 // const moreBtn = document.querySelectorAll('.more-btn')
 // const lessBtn = document.querySelectorAll('.less-btn')
 
-// const removeBtn = document.querySelector('.remove-btn')
-// //console.log(removeBtn)
-// for (let i = 0; i < removeBtn.length; i = +1) {
-//     var delBtn = removeBtn[i];
-//     delBtn.addEventListener('click', function (evt){
-//         const carItemsRevove = evt.target.dataset.products;
-//         //console.log(evt.target.dataset)
-//         if (products && products[carItemsRevove]) {
-//             delete products[carItemsRevove];
-//             localStorage.setItem(KEY, JSON.stringify(products));
-//             updataCart();
+// видалення конкретного продукту
+// можна оптимізувати
+containerCart.addEventListener('click', removeProduct)
+function removeProduct(event) {
+    if (event.target.className !== 'remove-btn') {
+        return;
+    }
 
-//         }
-//        updataCart
-//         localStorage.getItem('products');
-//         carItems = JSON.parse(carItems);
-//         Object.values(carItems).map(item => {
-//             item.id = 0;
+    const selectedItem = event.target.closest('.cart-list-item');
+    const selectedItemId = selectedItem.id;
+    const products = JSON.parse(localStorage.getItem(KEY));
+    const index = products.findIndex(item => item._id === selectedItemId);
+    
+    products.splice(index, 1);
+    localStorage.setItem(KEY, JSON.stringify(products));
+    selectedItem.remove();
 
-//             localStorage.removeItem('products')
-
-//         })
-//         location.reload()
-// })
-// }
+    if (products === null || products === undefined || products.length === 0) {
+        renderCart();
+        return;
+    }
+}
