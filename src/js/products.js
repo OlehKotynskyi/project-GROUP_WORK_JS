0fe7676e185403d354ab8414083e665985429b7b
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2023-12-25 09:26:40 +0200

    modification card + discount

--- src/js/products.js
+++ src/js/products.js
@@ -1,7 +1,7 @@
 import {
-  createMarkupProductsAll,
-  createMarkupPopularProducts,
-  createMarkupProductsDiscount,
+   createMarkupProductsAll,
+   createMarkupPopularProducts,
+   createMarkupProductsDiscount,
 } from './template.js';
 import { fetchProductsAll, fetchProducts } from './fetch.js';
 import addCounter from './cart-header-counter.js';
@@ -11,63 +11,63 @@ const containerDiscount = document.querySelector('.discount-container');
 const KEY = 'products in cart';
 
 async function renderAll() {
-  try {
-    const data = await fetchProductsAll('Fresh_Produce');
-     containerAll.insertAdjacentHTML('beforeend', createMarkupProductsAll(data));
-     addCounter();
-  } catch (error) {
-    console.log(error.message);
-  }
+   try {
+      const data = await fetchProductsAll('Fresh_Produce');
+      containerAll.insertAdjacentHTML('beforeend', createMarkupProductsAll(data));
+      addCounter();
+   } catch (error) {
+      console.log(error.message);
+   }
 }
 renderAll();
 
 async function renderPopular() {
-  try {
-    const data = await fetchProducts('popular');
-    containerPopular.insertAdjacentHTML(
-      'beforeend',
-      createMarkupPopularProducts(data)
-    );
-  } catch (error) {
-    console.log(error.message);
-  }
+   try {
+      const data = await fetchProducts('popular');
+      containerPopular.insertAdjacentHTML(
+         'beforeend',
+         createMarkupPopularProducts(data)
+      );
+   } catch (error) {
+      console.log(error.message);
+   }
 }
 renderPopular();
 
 async function renderDiscount() {
-  const data = await fetchProducts('discount');
-  containerDiscount.insertAdjacentHTML(
-    'beforeend',
-    createMarkupProductsDiscount(data)
-  );
+   const data = await fetchProducts('discount');
+   containerDiscount.insertAdjacentHTML(
+      'beforeend',
+      createMarkupProductsDiscount(data)
+   );
 }
 renderDiscount();
 
 containerAll.addEventListener('click', addBtnClick);
 
 async function addBtnClick(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
-  const selectedItem = event.target.closest('.list-item');
-  const selectedItemId = selectedItem.id;
-  console.log(selectedItemId);
-  try {
-    const currentProduct = await fetchProducts(selectedItemId);
-    const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
-    console.log(products);
-    const index = products.findIndex(item => item._id === selectedItemId);
-    console.log(index);
+   if (event.target.nodeName !== 'BUTTON') {
+      return;
+   }
+   const selectedItem = event.target.closest('.list-item');
+   const selectedItemId = selectedItem.id;
+   console.log(selectedItemId);
+   try {
+      const currentProduct = await fetchProducts(selectedItemId);
+      const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
+      console.log(products);
+      const index = products.findIndex(item => item._id === selectedItemId);
+      console.log(index);
 
-    if (index !== -1) {
-      products[index].quantity += 1;
-    } else {
-      currentProduct.quantity = 1;
-      products.push(currentProduct);
-    }
-    localStorage.setItem(KEY, JSON.stringify(products));
-    addCounter();
-  } catch (error) {
-    console.log(error.message);
-  }
+      if (index !== -1) {
+         products[index].quantity += 1;
+      } else {
+         currentProduct.quantity = 1;
+         products.push(currentProduct);
+      }
+      localStorage.setItem(KEY, JSON.stringify(products));
+      addCounter();
+   } catch (error) {
+      console.log(error.message);
+   }
 }
