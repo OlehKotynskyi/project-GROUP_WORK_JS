afaa1c6b5f377106d86fe20b356888118a08ddf6
TZaidel <113782124+TZaidel@users.noreply.github.com>
2023-12-26 16:49:38 +0000

    Deploying to gh-pages from @ OlehKotynskyi/project-GROUP_WORK_JS@912e5b65184fb3d7cdc1d5df3fa7646b84397313 🚀

--- assets/scroll-5c781e52.js
+++ assets/scroll-5c781e52.js
@@ -75,4 +75,4 @@
                 </button>
               </div>
         </li>`).join("")}const v="products in cart";function S(){const i=document.querySelector(".header-basket-counter"),e=JSON.parse(localStorage.getItem(v));e===null||e.length===0?i.textContent="0":i.textContent=`${e.length}`}let l=0;const d=60,p=document.querySelector(".header"),u=document.querySelector(".header-section"),r=()=>window.pageYOffset||document.documentElement.scrollTop,m=()=>p.classList.contains("hide");function O(){window.addEventListener("scroll",()=>{r()>l&&!m()&&r()>d?p.classList.add("hide"):r()<l&&m()&&p.classList.remove("hide"),r()>d?u.classList.add("scrolled"):u.classList.remove("scrolled"),l=r()})}export{S as a,$ as b,w as c,y as d,k as e,O as h};
-//# sourceMappingURL=scroll-e03d10dd.js.map
+//# sourceMappingURL=scroll-5c781e52.js.map
