1434a8c314aeffad0cb4e9fb77a46e3e11fe1eb7
Mahavr <mahavr2122003@gmail.com>
2023-12-26 18:28:14 +0200

    fix filters and add pagination withouth styles

--- src/js/products.js
+++ src/js/products.js
@@ -14,72 +14,77 @@ const KEY = 'products in cart';
 
 // Функція для оновлення списку продуктів
 export function updateProductsList(products) {
-   const container = document.querySelector('.products-container');
-   // Перевіряємо, що products не є null і має властивість length
-   if (!products || products.length === 0) {
-       container.innerHTML = `<p>Nothing was found for the selected filters...</p>
+  const container = document.querySelector('.products-container');
+  // Перевіряємо, що products не є null і має властивість length
+  if (!products || products.length === 0) {
+    container.innerHTML = `<p>Nothing was found for the selected filters...</p>
                               <p>Try adjusting your search parameters or browse our range by other criteria to find the perfect product for you.</p>`;
-   } else {
-       container.innerHTML = createMarkupProductsAll(products);
-   }
- }
- export function getProductsLimit() {
-   const screenWidth = window.innerWidth;
-   if (screenWidth < 375) { // Мобільні пристрої
-       return 6;
-   } else if (screenWidth >= 375 && screenWidth < 768) { // Таблет
-       return 8;
-   } else { // Десктоп і вище
-       return 9;
-   }
- }
+  } else {
+    container.innerHTML = createMarkupProductsAll(products);
+   
+  }
+}
+export function getProductsLimit() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth < 768) {
+    // Мобільні пристрої
+    return 6;
+  } else if (screenWidth >= 768 && screenWidth < 1440) {
+    // Таблет
+    return 8;
+  } else {
+    // Десктоп і вище
+    return 9;
+  }
+}
 
+// async function renderAll() {
+//   try {
+//     const data = await fetchProductsAll('Fresh_Produce');
+//     containerAll.insertAdjacentHTML(
+//       'beforeend',
+//       createMarkupProductsAll(products)
+//     );
+//     addCounter();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+// renderAll();
 
-async function renderAll() {
+// перемішування масиву та вибору випадкових продуктів
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Перемішування елементів
+  }
+}
+
+function getRandomProducts(products, count) {
+  shuffleArray(products);
+  return products.slice(0, count);
+}
+
+async function renderPopular() {
   try {
-    const data = await fetchProductsAll('Fresh_Produce');
-    containerAll.insertAdjacentHTML('beforeend', createMarkupProductsAll(data));
-    addCounter();
+    const data = await fetchProducts('popular');
+    const randomData = getRandomProducts(data, 5); // Вибірка 5 випадкових продуктів
+    containerPopular.innerHTML = createMarkupPopularProducts(randomData);
   } catch (error) {
     console.log(error.message);
   }
 }
-renderAll();
 
-
-// перемішування масиву та вибору випадкових продуктів
-function shuffleArray(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-       [array[i], array[j]] = [array[j], array[i]]; // Перемішування елементів
-   }
-}
-
-function getRandomProducts(products, count) {
-   shuffleArray(products);
-   return products.slice(0, count);
-}
-
-async function renderPopular() {
-   try {
-      const data = await fetchProducts('popular');
-      const randomData = getRandomProducts(data, 5); // Вибірка 5 випадкових продуктів
-      containerPopular.innerHTML = createMarkupPopularProducts(randomData);
-  } catch (error) {
-       console.log(error.message);
-      }
-}
-
-renderPopular()
+renderPopular();
 
 async function renderDiscount() {
-   try {
-      const data = await fetchProducts('discount');
-      const randomData = getRandomProducts(data, 2); // Вибірка 2 випадкових продуктів
-      containerDiscount.innerHTML = createMarkupProductsDiscount(randomData);
+  try {
+    const data = await fetchProducts('discount');
+    const randomData = getRandomProducts(data, 2); // Вибірка 2 випадкових продуктів
+    containerDiscount.innerHTML = createMarkupProductsDiscount(randomData);
   } catch (error) {
-       console.log(error.message);
-       }
+    console.log(error.message);
+  }
 }
 
 renderDiscount();
@@ -155,15 +160,13 @@ async function addBtnClickDiscount(event) {
   return;
 }
 
-
 // Функція для видалення підкреслення між словами
 export function removeUnderscores(arr) {
-   return arr.map(obj => {
-     let category = obj.category;
-     if (typeof category === 'string') {
-       category = category.split('_').join(' ');
-     }
-     return { ...obj, category };
-   });
- }
-
+  return arr.map(obj => {
+    let category = obj.category;
+    if (typeof category === 'string') {
+      category = category.split('_').join(' ');
+    }
+    return { ...obj, category };
+  });
+}
