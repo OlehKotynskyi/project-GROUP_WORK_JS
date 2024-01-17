b849bcc2db4438a46683e00384c2cbab8434c1a9
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-01-17 11:43:33 +0200

    модивікація секцій

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,7 @@
 import * as basicLightBox from 'basiclightbox';
-import closed from '../img/svg/closed.svg'
-import frutX1 from "../img/basket-s-popap/frut-popap-x1.png";
-import frutX2 from "../img/basket-s-popap/frut-popap-x2.png";
+import closed from '../img/svg/closed.svg';
+import frutX1 from '../img/basket-s-popap/frut-popap-x1.png';
+import frutX2 from '../img/basket-s-popap/frut-popap-x2.png';
 const form = document.querySelector('#formEmail');
 const emailInput = document.querySelector('#formInput');
 
@@ -9,52 +9,47 @@ form.addEventListener('submit', handleFormSubmit);
 // emailInput.addEventListener('keyup', handleKeyPress);
 
 function handleFormSubmit(event) {
-   event.preventDefault();
-   const email = emailInput.value.trim();
+  event.preventDefault();
+  const email = emailInput.value.trim();
 
-   if (!validateEmail(email)) {
-      alert('Please enter a valid email address');
-      return;
-   }
+  if (!validateEmail(email)) {
+    alert('Please enter a valid email address');
+    return;
+  }
 
-   if (email === '') {
-      alert('Please enter an email address');
-      return;
-   }
+  if (email === '') {
+    alert('Please enter an email address');
+    return;
+  }
 
-   sendDataToServer({ email })
-      .then(() => {
-         console.log('Data sent successfully');
-         emailInput.value = '';
-         subModal();
-      })
-      .catch(error => {
-         console.error('There was a problem with the fetch operation:', error);
-      });
+  sendDataToServer({ email })
+    .then(() => {
+      console.log('Data sent successfully');
+      emailInput.value = '';
+      subModal();
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
-// function handleKeyPress(event) {
-//    if (event.key === 'Enter') {
-//       handleFormSubmit(event);
-//    }
-// }
-
 function sendDataToServer(data) {
-   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-         console.log('Imitating sending data to server:', data);
-         resolve();
-      }, 2000);
-   });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Imitating sending data to server:', data);
+      resolve();
+    }, 2000);
+  });
 }
 
 function validateEmail(email) {
-   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-   return emailRegex.test(email);
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
 }
 
 function subModal() {
-   const instance = basicLightBox.create(`
+  const instance = basicLightBox.create(
+    `
    <div class="sub-modal">
       <button class="remove-btn">
          <img class="remove-btn-img" src="${closed}" alt="icon bascket" width="18" height="18">
@@ -70,20 +65,22 @@ function subModal() {
          srcset="${frutX2}"
       />
    </div>
-   `, {
-      onClose: (instance) => subModal.removeEventListener('click', modalClose)
-   })
-   instance.show();
-   const subModal = document.querySelector('.sub-modal');
-   subModal.addEventListener('click', modalClose);
+   `,
+    {
+      onClose: instance => subModal.removeEventListener('click', modalClose),
+    }
+  );
+  instance.show();
+  const subModal = document.querySelector('.sub-modal');
+  subModal.addEventListener('click', modalClose);
 
-   function modalClose(evt) {
-      if (
-         evt.target.className === 'remove-btn'
-         || evt.target.className === 'remove-btn-img'
-      ) {
-         instance.close();
-      }
-      return;
-   }
+  function modalClose(evt) {
+    if (
+      evt.target.className === 'remove-btn' ||
+      evt.target.className === 'remove-btn-img'
+    ) {
+      instance.close();
+    }
+    return;
+  }
 }
--- src/js/products.js
+++ src/js/products.js
@@ -1,7 +1,7 @@
 import {
-   createMarkupProductsAll,
-   createMarkupPopularProducts,
-   createMarkupProductsDiscount,
+  createMarkupProductsAll,
+  createMarkupPopularProducts,
+  createMarkupProductsDiscount,
 } from './template.js';
 import check from '../img/svg/check.svg';
 import { fetchProductsAll, fetchProducts } from './fetch.js';
@@ -15,145 +15,123 @@ const KEY = 'products in cart';
 
 // Функція для оновлення списку продуктів
 export function updateProductsList(products) {
-   const container = document.querySelector('.products-container');
-   if (!products || products.length === 0) {
-      // Якщо продуктів немає, показуємо повідомлення
-      container.innerHTML = `<div class="cart-empty empty-filter">
+  const container = document.querySelector('.products-container');
+  if (!products || products.length === 0) {
+    // Якщо продуктів немає, показуємо повідомлення
+    container.innerHTML = `<div class="cart-empty empty-filter">
            <h3 class="products-titel">Nothing was found for the selected <span>filters...</span></h3>
            <p class="filters-text-not-found">Try adjusting your search parameters or browse our range by other criteria to find the perfect product for you.</p>
        </div>`;
-   } else {
-      // Якщо продукти є, заповнюємо контейнер продуктами і приховуємо повідомлення, якщо воно було показано
-      container.innerHTML = createMarkupProductsAll(products);
-   }
+  } else {
+    // Якщо продукти є, заповнюємо контейнер продуктами і приховуємо повідомлення, якщо воно було показано
+    container.innerHTML = createMarkupProductsAll(products);
+  }
 }
 
 export function getProductsLimit() {
-   const screenWidth = window.innerWidth;
-   if (screenWidth < 768) {
-      // Мобільні пристрої
-      return 6;
-   } else if (screenWidth >= 768 && screenWidth < 1440) {
-      // Таблет
-      return 8;
-   } else if (screenWidth > 1440) {
-      // Десктоп і вище
-      return 9;
-   }
-
+  const screenWidth = window.innerWidth;
+  if (screenWidth < 768) {
+    // Мобільні пристрої
+    return 6;
+  } else if (screenWidth >= 768 && screenWidth < 1440) {
+    // Таблет
+    return 8;
+  } else if (screenWidth > 1440) {
+    // Десктоп і вище
+    return 9;
+  }
 }
 // renderAll();
 
 // перемішування масиву та вибору випадкових продуктів
 function shuffleArray(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Перемішування елементів
-   }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Перемішування елементів
+  }
 }
 
 async function renderPopular() {
-   try {
-      const data = await fetchProducts('popular');
-      const newData = removeUnderscores(data);
-      const randomData = getRandomProducts(newData, 5); // Вибірка 5 випадкових продуктів
-      containerPopular.innerHTML = createMarkupPopularProducts(randomData);
-   } catch (error) {
-      console.log(error.message);
-   }
+  try {
+    const data = await fetchProducts('popular');
+    const newData = removeUnderscores(data);
+    const randomData = getRandomProducts(newData, 5); // Вибірка 5 випадкових продуктів
+    containerPopular.innerHTML = createMarkupPopularProducts(randomData);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 renderPopular();
 
 async function renderAll() {
-   try {
-      const data = await fetchProductsAll('Fresh_Produce');
-      containerAll.insertAdjacentHTML(
-         'beforeend',
-         createMarkupProductsAll(removeUnderscores(data))
-      );
-      addCounter();
-      // console.log(data)
-
-   } catch (error) {
-      console.log(error.message);
-   }
+  try {
+    const data = await fetchProductsAll('Fresh_Produce');
+    containerAll.insertAdjacentHTML(
+      'beforeend',
+      createMarkupProductsAll(removeUnderscores(data))
+    );
+    addCounter();
+    // console.log(data)
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-// renderAll();
-
-// async function renderAll() {
-//   try {
-//     const data = await fetchProductsAll('Fresh_Produce');
-//     containerAll.insertAdjacentHTML(
-//       'beforeend',
-//       createMarkupProductsAll(products)
-//     );
-//     addCounter();
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// renderAll();
-
 function getRandomProducts(products, count) {
-   shuffleArray(products);
-   return products.slice(0, count);
+  shuffleArray(products);
+  return products.slice(0, count);
 }
 
 async function renderDiscount() {
-   try {
-      const data = await fetchProducts('discount');
-      const randomData = getRandomProducts(data, 2); // Вибірка 2 випадкових продуктів
-      containerDiscount.innerHTML = createMarkupProductsDiscount(randomData);
-   } catch (error) {
-      console.log(error.message);
-   }
-   return;
+  try {
+    const data = await fetchProducts('discount');
+    const randomData = getRandomProducts(data, 2); // Вибірка 2 випадкових продуктів
+    containerDiscount.innerHTML = createMarkupProductsDiscount(randomData);
+  } catch (error) {
+    console.log(error.message);
+  }
+  return;
 }
 
 containerDiscount.addEventListener('click', addBtnClickDiscount);
 
 export async function addBtnClickDiscount(event) {
-   if (
-      event.target.className === 'discount-link-basket' ||
-      event.target.className === 'discount-basket-icon' ||
-      event.target.className === 'discount-basket-icon'
-      // event.target.className === 'add-btn'
-      // event.target.nodeName === 'BUTTON' ||
-      // event.target.nodeName === 'SPAN' ||
-      //   event.target.nodeName === 'IMG'
-   ) {
-      const selectedItem = event.target.closest('.discount-list-item');
+  if (
+    event.target.className === 'discount-link-basket' ||
+    event.target.className === 'discount-basket-icon' ||
+    event.target.className === 'discount-basket-icon'
+  ) {
+    const selectedItem = event.target.closest('.discount-list-item');
 
-      const selectedItemId = selectedItem.id;
+    const selectedItemId = selectedItem.id;
 
-      try {
-         const currentProduct = await fetchProducts(selectedItemId);
-         const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
+    try {
+      const currentProduct = await fetchProducts(selectedItemId);
+      const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
 
-         const index = products.findIndex(item => item._id === selectedItemId);
+      const index = products.findIndex(item => item._id === selectedItemId);
 
-         if (index !== -1) {
-            products[index].quantity += 0;
-         } else {
-            currentProduct.quantity = 1;
-            products.push(currentProduct);
-            const button = selectedItem.querySelector('button');
-            button.disabled = true;
-            button.innerHTML = `<span class="icon-styles">
+      if (index !== -1) {
+        products[index].quantity += 0;
+      } else {
+        currentProduct.quantity = 1;
+        products.push(currentProduct);
+        const button = selectedItem.querySelector('button');
+        button.disabled = true;
+        button.innerHTML = `<span class="icon-styles">
                      <img class="discount-basket-icon" src="${check}" alt="icon bascket" width="18" height="18">
 
                   </span>`;
-            button.classList.add('disabled');
-         }
-         localStorage.setItem(KEY, JSON.stringify(products));
-         addCounter();
-      } catch (error) {
-         console.log(error.message);
+        button.classList.add('disabled');
       }
-   }
-   return;
+      localStorage.setItem(KEY, JSON.stringify(products));
+      addCounter();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return;
 }
 
 renderDiscount();
@@ -161,89 +139,88 @@ renderDiscount();
 containerAll.addEventListener('click', addBtnClick);
 
 export async function addBtnClick(event) {
-   if (
-      event.target.nodeName === 'BUTTON' ||
-      event.target.className === 'add-btn' ||
-      event.target.nodeName === 'IMG'
-   ) {
-      //     return;
-      //   }
-      const selectedItem = event.target.closest('.list-item-body-price');
+  if (
+    event.target.nodeName === 'BUTTON' ||
+    event.target.className === 'add-btn' ||
+    event.target.nodeName === 'IMG'
+  ) {
+    const selectedItem = event.target.closest('.list-item-body-price');
 
+    // Проверяем, есть ли родительский элемент
+    if (selectedItem && selectedItem.parentElement) {
       const selectedItemId = selectedItem.parentElement.id;
-      //const selectedItemId = selectedItem.id;
 
       try {
-         const currentProduct = await fetchProducts(selectedItemId);
-         const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
+        const currentProduct = await fetchProducts(selectedItemId);
+        const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
 
-         const index = products.findIndex(item => item._id === selectedItemId);
+        const index = products.findIndex(item => item._id === selectedItemId);
 
-         if (index !== -1) {
-            products[index].quantity += 0;
-         } else {
-            currentProduct.quantity = 1;
-            products.push(currentProduct);
-            //==========
-            const button = selectedItem.querySelector('button');
-            button.disabled = true;
-            button.innerHTML = `<img src="${check}" alt="icon check" width="18" height="18">`;
-            button.classList.add('disabled');
-            //=========
-         }
-         localStorage.setItem(KEY, JSON.stringify(products));
-         addCounter();
+        if (index !== -1) {
+          products[index].quantity += 0;
+        } else {
+          currentProduct.quantity = 1;
+          products.push(currentProduct);
+          //==========
+          const button = selectedItem.querySelector('button');
+          button.disabled = true;
+          button.innerHTML = `<img src="${check}" alt="icon check" width="18" height="18">`;
+          button.classList.add('disabled');
+          //=========
+        }
+        localStorage.setItem(KEY, JSON.stringify(products));
+        addCounter();
       } catch (error) {
-         console.error();
+        console.error(error.message);
       }
-   }
-   return;
+    }
+  }
+  return;
 }
-
 
 containerPopular.addEventListener('click', addBtnClickPopularCard);
 
 async function addBtnClickPopularCard(event) {
-   if (
-      event.target.nodeName === 'BUTTON' ||
-      event.target.className === 'popular-basket-img'
-   ) {
-      const selectedItem = event.target.closest('.product-popular-card');
-      const selectedItemId = selectedItem.id;
-      const button = selectedItem.querySelector('button');
-      button.disabled = true;
+  if (
+    event.target.nodeName === 'BUTTON' ||
+    event.target.className === 'popular-basket-img'
+  ) {
+    const selectedItem = event.target.closest('.product-popular-card');
+    const selectedItemId = selectedItem.id;
+    const button = selectedItem.querySelector('button');
+    button.disabled = true;
 
-      try {
-         const currentProduct = await fetchProducts(selectedItemId);
-         const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
+    try {
+      const currentProduct = await fetchProducts(selectedItemId);
+      const products = JSON.parse(localStorage.getItem(KEY)) ?? [];
 
-         const index = products.findIndex(item => item._id === selectedItemId);
+      const index = products.findIndex(item => item._id === selectedItemId);
 
-         if (index !== -1) {
-            products[index].quantity += 0;
-         } else {
-            currentProduct.quantity = 1;
-            products.push(currentProduct);
-            button.innerHTML = `<img class="popular-disadbled-img" src="${check}" alt="icon bascket" width="20" height="20">`;
-            button.classList.add('popular-disadbled-btn');
-         }
-         localStorage.setItem(KEY, JSON.stringify(products));
-         addCounter();
-      } catch (error) {
-         console.log(error.message);
-         button.disabled = false;
+      if (index !== -1) {
+        products[index].quantity += 0;
+      } else {
+        currentProduct.quantity = 1;
+        products.push(currentProduct);
+        button.innerHTML = `<img class="popular-disadbled-img" src="${check}" alt="icon bascket" width="20" height="20">`;
+        button.classList.add('popular-disadbled-btn');
       }
-   }
-   return;
+      localStorage.setItem(KEY, JSON.stringify(products));
+      addCounter();
+    } catch (error) {
+      console.log(error.message);
+      button.disabled = false;
+    }
+  }
+  return;
 }
 
 // Функція для видалення підкреслення між словами
 export function removeUnderscores(arr) {
-   return arr.map(obj => {
-      let category = obj.category;
-      if (typeof category === 'string') {
-         category = category.split('_').join(' ');
-      }
-      return { ...obj, category };
-   });
+  return arr.map(obj => {
+    let category = obj.category;
+    if (typeof category === 'string') {
+      category = category.split('_').join(' ');
+    }
+    return { ...obj, category };
+  });
 }
